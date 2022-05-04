2ec14658097cfc607fabd98fdf5967d060c88046
Anushka Hebbar <56372418+anushkahebbar@users.noreply.github.com>
2022-05-04 16:52:09 +0530

    Set root filesys

---
+++ 3_set_guest_rootfs.sh
@@ -0,0 +1,16 @@
+#!bin/bash 
+
+# Script to define where the rootfs for the microVM is on the host machine
+
+rootfs_path=$(pwd)"/resources/$2-rootfs.ext4"
+
+curl --unix-socket /tmp/firecracker.socket -i \
+  -X PUT 'http://localhost/drives/rootfs' \
+  -H 'Accept: application/json'           \
+  -H 'Content-Type: application/json'     \
+  -d "{
+        \"drive_id\": \"rootfs\",
+        \"path_on_host\": \"${rootfs_path}\",
+        \"is_root_device\": true,
+        \"is_read_only\": false
+   }"
